700b7511e88fe28d445b3ff44ed3e99debfa0224
bob <praveenprakasham@gmail.com>
2019-07-31 22:11:28 +0530

    Updates

---
+++ precache-manifest.dc444a19b1800ecaaf019a29c8157e64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9603fd5726aed514092efdca1af2d3",
+    "url": "/react_venue/index.html"
+  },
+  {
+    "revision": "cdf9e9572e0f828ca3e0",
+    "url": "/react_venue/static/css/main.53f8c940.chunk.css"
+  },
+  {
+    "revision": "354d381a0d3a13e519f0",
+    "url": "/react_venue/static/js/2.dae01f07.chunk.js"
+  },
+  {
+    "revision": "cdf9e9572e0f828ca3e0",
+    "url": "/react_venue/static/js/main.0238f7ea.chunk.js"
+  },
+  {
+    "revision": "38a24767a3eed7ee95df",
+    "url": "/react_venue/static/js/runtime~main.2375cffa.js"
+  },
+  {
+    "revision": "45c81405176479291c3f9ea8004ef212",
+    "url": "/react_venue/static/media/slide_one.45c81405.jpg"
+  },
+  {
+    "revision": "78a0f544930ca67e6c985147aef671c3",
+    "url": "/react_venue/static/media/slide_three.78a0f544.jpg"
+  },
+  {
+    "revision": "70cf2a4512474195640413f23bbf9523",
+    "url": "/react_venue/static/media/slide_two.70cf2a45.jpg"
+  }
+]);
